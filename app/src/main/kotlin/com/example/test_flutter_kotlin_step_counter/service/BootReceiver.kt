914ea965a3d6808bf342a0c38b47f1f5e7a74a9c
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-06-23 12:57:30 +0900

    commit 20250623 リブート設定

---
+++ app/src/main/kotlin/com/example/test_flutter_kotlin_step_counter/service/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.test_flutter_kotlin_step_counter.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("BootReceiver", "✅ 端末再起動後：BOOT_COMPLETED受信")
+
+            val serviceIntent = Intent(context, StepUpdateService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
